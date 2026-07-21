dc76d4f21343e2ab5df55367532e9a78d87e94ff
LixdyDev <anurarohana1239@gmail.com>
2026-07-21 21:50:00 +0530

    complete the dice roller program

--- dice roller program/dice_roller program.py
+++ dice roller program/dice_roller program.py
@@ -1,3 +1,5 @@
+import random
+
 dice_art = {
     1: ("┌─────────┐",
         "│         │",
@@ -31,3 +33,18 @@ dice_art = {
         "└─────────┘")
 }
 
+dice = []
+total = 0
+num_of_dice = int(input("How many dice?: "))
+
+for die in range(num_of_dice):
+    dice.append(random.randint(1, 6))
+
+for die in range(num_of_dice):
+    for line in dice_art.get(dice[die]):
+        print(line)
+
+
+for die in dice:
+    total += die
+print(f"total: {total}")
